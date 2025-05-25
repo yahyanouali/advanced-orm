692a2846c8e330898ce17215db72b0f52c831fe6
Yahya-Leno <y.nouali@gmail.com>
2025-05-25 16:34:46 +0200

    Added custom formatter for hibernate sql

--- src/main/java/org/acme/resource/ArtisteResource.java
+++ src/main/java/org/acme/resource/ArtisteResource.java
@@ -3,6 +3,7 @@ package org.acme.resource;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import lombok.extern.slf4j.Slf4j;
 import org.acme.entities.Artiste;
 import org.acme.service.ArtisteService;
@@ -10,17 +11,25 @@ import org.acme.service.ArtisteService;
 import java.util.List;
 
 @Slf4j
-@Path("")
+@Path("/artists")
 public class ArtisteResource {
     @Inject
     ArtisteService artisteService;
 
     @GET
-    @Path("/artists")
+    @Path("/")
     public List<Artiste> getArtists() {
         var artistes = artisteService.getArtistes();
         log.info("Artistes trouvés : {}", artistes);
         return artistes;
+    }
+
+    @GET
+    @Path("/{id}")
+    public Artiste getArtistById(@PathParam("id") Integer id) {
+        var artiste = artisteService.getArtisteById(id);
+        log.info("Artiste trouvé : {}", artiste);
+        return artiste.orElse(null);
     }
 
 }
--- src/main/java/org/acme/service/ArtisteService.java
+++ src/main/java/org/acme/service/ArtisteService.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.acme.entities.Artiste;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @ApplicationScoped
@@ -19,5 +20,11 @@ public class ArtisteService {
     public List<Artiste> getArtistes() {
         TypedQuery<Artiste> query = em.createQuery("from Artiste", Artiste.class);
         return query.getResultList();
+    }
+
+    public Optional<Artiste> getArtisteById(Integer id) {
+        TypedQuery<Artiste> query = em.createQuery("select a from Artiste a where a.id = ?1", Artiste.class);
+        query.setParameter(1, id);
+        return Optional.of(query.getSingleResult());
     }
 }
